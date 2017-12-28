c102b3cf541179c545d522d3f71faf8643e9f819
chenchen2018 <chenchengynx@gmail.com>
2017-12-28 15:01:03 -0500

    Q19: add solution

---
+++ src/leetcode/medium/Q19.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+/**
+ * Created by chen on 12/28/17.
+ */
+public class Q19 {
+}
+
+class Q19Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
