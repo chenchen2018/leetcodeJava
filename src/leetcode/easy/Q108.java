33293ce4037d573139ebed2d1e0c9f6d7975af79
chenchen2018 <chenchengynx@gmail.com>
2017-12-30 15:12:15 -0500

    Q108: add solution

---
+++ src/leetcode/easy/Q108.java
@@ -0,0 +1,27 @@
+package leetcode.easy;
+
+/**
+ * Created by chen on 12/30/17.
+ */
+public class Q108 {
+}
+
+class Q108Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, left, mid - 1);
+        root.right = helper(nums, mid + 1, right);
+        return root;
+    }
+}
