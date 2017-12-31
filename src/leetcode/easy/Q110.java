6b6a5c2c349dbdf2fe46c182b5a46f991523645d
chenchen2018 <chenchengynx@gmail.com>
2017-12-31 11:10:09 -0500

    Q110: add another solution

--- src/leetcode/easy/Q110.java
+++ src/leetcode/easy/Q110.java
@@ -27,6 +27,21 @@ class Q110Solution {
             this.isBalanced = isBalanced;
         }
     }
+
+    public boolean isBalanced2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftHeight = getHeight(root.left), rightHeight = getHeight(root.right);
+        return Math.abs(leftHeight - rightHeight) <= 1 && isBalanced2(root.left) && isBalanced2(root.right);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    }
 }
 
 class TreeNode {
