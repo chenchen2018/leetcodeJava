5c9ffaa2ac0b1c8e21c1b0c00e9cd1cb0b9685b7
chenchen2018 <chenchengynx@gmail.com>
2017-12-31 17:59:12 -0500

    Q32: fix typo

--- src/leetcode/hard/Q32.java
+++ src/leetcode/hard/Q32.java
@@ -7,7 +7,7 @@ import java.util.Stack;
  */
 public class Q32 {
     public static void main(String[] args) {
-        System.out.println(new Q32Solution().longestValidParentheses2("(()"););
+        System.out.println(new Q32Solution().longestValidParentheses2("(()"));
     }
 }
 
