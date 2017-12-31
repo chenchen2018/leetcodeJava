1d61c0baf1f73e2a7b3adc9610ba59c1e2921dca
chenchen2018 <chenchengynx@gmail.com>
2017-12-31 11:27:30 -0500

    Q111: add solution

--- src/leetcode/easy/Q111.java
+++ src/leetcode/easy/Q111.java
@@ -1,11 +1,57 @@
 package leetcode.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Q111 {
 
 }
 
 class Q111Solution {
     public int minDepth(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        return helper(root);
+    }
+
+    private int helper(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        if (node.left == null) {
+            return 1 + helper(node.right);
+        }
+        if (node.right == null) {
+            return 1 + helper(node.left);
+        }
+        return 1 + Math.min(helper(node.left), helper(node.right));
+    }
+
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> currLevel = new LinkedList<>();
+        currLevel.offer(root);
+        int level = 0;
+        while (!currLevel.isEmpty()) {
+            level++;
+            Queue<TreeNode> nextLevel = new LinkedList<>();
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.poll();
+                if (node.left == null && node.right == null) {
+                    return level;
+                }
+                if (node.left != null) {
+                    nextLevel.offer(node.left);
+                }
+                if (node.right != null) {
+                    nextLevel.offer(node.right);
+                }
+            }
+            currLevel = nextLevel;
+        }
+        return level;
     }
 }
