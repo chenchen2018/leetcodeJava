0921d585215d47ec77e79950ef367d7cda1f4bc3
Chen Chen <Chen.Chen@tradingscreen.com>
2018-01-11 17:10:11 -0500

    Q102: add method declaration

---
+++ src/leetcode/medium/Q102.java
@@ -0,0 +1,14 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q102 {
+}
+
+class Q102Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+    }
+}
