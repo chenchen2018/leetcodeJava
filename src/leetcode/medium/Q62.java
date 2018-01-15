c52985483835636066109268b1e5576a67b62c30
Chen Chen <cchen@Chens-MacBook-Pro.local>
2018-01-14 22:19:17 -0500

    Q62: add more elegant solution: rotating array

--- src/leetcode/medium/Q62.java
+++ src/leetcode/medium/Q62.java
@@ -26,4 +26,63 @@ class Q62Solution {
         }
         return dp[m - 1][n - 1];
     }
+
+    //version 2: rotate array
+    public int uniquePaths2(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (m == 1) {
+            return 1;
+        }
+        int[] prev = new int[n], curr = new int[n];
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row == 0 && col == 0) {
+                    curr[col] = 1;
+                    continue;
+                }
+                if (row == 0) {
+                    curr[col] = curr[col - 1];
+                } else if (col == 0) {
+                    curr[row] = prev[row - 1];
+                } else {
+                    curr[col] = curr[col - 1] + prev[col];
+                }
+            }
+            prev = curr;
+        }
+        return curr[n - 1];
+    }
+
+    //version 3: fashion way of 滚动数组
+    public int uniquePath3(int m, int n) {
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        if (m == 1) {
+            return 1;
+        }
+        int[][] f = new int[2][n];
+        int prev = 0, curr = 1;
+        for (int row = 0; row < m; row++) {
+            prev = curr;
+            curr = 1 - curr;
+            for (int col = 0; col < n; col++) {
+                f[curr][col] = 0;
+                if (row == 0 && col == 0) {
+                    f[curr][col] = 1;
+                    continue;
+                }
+                if (row == 0) {
+                    f[curr][col] = f[curr][col - 1];
+                } else if (col == 0) {
+                    f[curr][col] = f[prev][col];
+                } else {
+                    f[curr][col] = f[curr][col - 1] + f[prev][col];
+                }
+            }
+        }
+        return f[curr][n - 1];
+    }
 }
