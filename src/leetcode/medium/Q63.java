d7f8a2942f633399b6e9abe6d2d53c236c6bdde0
Chen Chen <Chen.Chen@tradingscreen.com>
2018-01-22 13:03:50 -0500

    Q63: add rotating array and 1-d array solution

--- src/leetcode/medium/Q63.java
+++ src/leetcode/medium/Q63.java
@@ -44,4 +44,67 @@ class Q63Solution {
         }
         return dp[rows - 1][cols - 1];
     }
+
+    //rotating array
+    public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        int[][] f = new int[2][cols];
+
+        int now = 0, old = 1;
+        for (int row = 0; row < rows; row++) {
+            old = now;
+            now = 1 - now;
+            for (int col = 0; col < cols; col++) {
+                f[now][col] = 0;
+                if (row == 0 && col == 0) {
+                    f[now][col] = obstacleGrid[0][0] == 0 ? 1 : 0;
+                    continue;
+                }
+                if (obstacleGrid[row][col] == 0) {
+                    if (row > 0) {
+                        f[now][col] += f[old][col];
+                    }
+                    if (col > 0) {
+                        f[now][col] += f[now][col - 1];
+                    }
+                }
+            }
+        }
+        return f[now][cols - 1];
+    }
+
+    //only one array!
+    public int uniquePathsWithObstacles3(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = obstacleGrid.length, cols = obstacleGrid[0].length;
+        int[] f = new int[cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (row == 0 && col == 0) {
+                    f[col] = obstacleGrid[0][0] == 0 ? 1 : 0;
+                    continue;
+                }
+                if (obstacleGrid[row][col] == 0) {
+                    if (row == 0) {
+                        f[col] = f[col - 1];
+                    } else if (col == 0) {
+                        //do nothing
+                    } else {
+                        f[col] += f[col - 1];
+                    }
+                } else {
+                    f[col] = 0;
+                }
+            }
+        }
+        return f[cols - 1];
+    }
 }
