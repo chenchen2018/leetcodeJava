986a7d9be2674fa05923f772d41d51f8c117d721
Chen Chen <Chen.Chen@tradingscreen.com>
2018-01-22 14:53:31 -0500

    Q256: add more elegant dp solutions

--- src/leetcode/easy/Q256.java
+++ src/leetcode/easy/Q256.java
@@ -19,4 +19,51 @@ class Q256Solution {
         }
         return Math.min(Math.min(dp[costs.length - 1][0], dp[costs.length - 1][1]), dp[costs.length - 1][2]);
     }
+
+    //用序列型做一下
+    public int minCost2(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        int houses = costs.length;
+        int[][] f = new int[houses + 1][3];
+        for (int house = 1; house <= houses; house++) {
+            for (int color = 0; color < costs[0].length; color++) {
+                f[house][color] = Integer.MAX_VALUE;
+                for (int prevColor = 0; prevColor < costs[0].length; prevColor++) {
+                    if (color != prevColor) {
+                        f[house][color] = Math.min(f[house][color], f[house - 1][prevColor] + costs[house - 1][color]);
+                    }
+                }
+            }
+        }
+        return Math.min(f[houses][0], Math.min(f[houses][1], f[houses][2]));
+    }
+
+    //还是序列型 但是这次只用两个数组
+    public int minCost3(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+
+        int houses = costs.length;
+        int[][] f = new int[2][3];
+
+        int old = 1, now = 0;
+
+        for (int house = 1; house <= houses; house++) {
+            old = now;
+            now = 1 - now;
+            for (int color = 0; color < 3; color++) {
+                f[now][color] = Integer.MAX_VALUE;
+                for (int prevColor = 0; prevColor < 3; prevColor++) {
+                    if (color != prevColor) {
+                        f[now][color] = Math.min(f[now][color], f[old][prevColor] + costs[house - 1][color]);
+                    }
+                }
+            }
+        }
+
+        return Math.min(f[now][0], Math.min(f[now][1], f[now][2]));
+    }
 }
