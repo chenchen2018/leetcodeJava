4dd89e5ca3c7d5d17596af76354cf0818af892ed
Chen Chen <cchen@Chens-MacBook-Pro.local>
2018-01-27 12:44:26 -0500

    Q300: add solution

---
+++ src/leetcode/medium/Q300.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+/**
+ * Created by cchen on 1/27/18.
+ */
+public class Q300 {
+}
+
+class Q300Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int res = 0;
+        int[] f = new int[n];
+        for (int i = 0; i < n; i++) {
+            f[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            res = Math.max(res, f[i]);
+        }
+        return res;
+    }
+}
