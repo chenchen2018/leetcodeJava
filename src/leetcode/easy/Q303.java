36f96a54b8281f32199003d2bff77c2f471c3419
Chen Chen <cchen@Chens-MacBook-Pro.local>
2018-02-07 20:53:33 -0500

    Q303: add solution

---
+++ src/leetcode/easy/Q303.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+/**
+ * Created by cchen on 2/7/18.
+ */
+public class Q303 {
+}
+
+class NumArray {
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
