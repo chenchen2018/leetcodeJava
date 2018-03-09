fe1a8d6ecb7dfb5fdc362abf432ac57b240e55ef
Chen Chen <Chen.Chen@tradingscreen.com>
2018-03-09 17:34:49 -0500

    Q521: add my own string generator solution

--- src/leetcode/easy/Q521.java
+++ src/leetcode/easy/Q521.java
@@ -12,7 +12,9 @@ public class Q521 {
 class Q521Solution {
 
     public void test(String s) {
-        for (String string : stringGenerator(s)) {
+        List<String> res = stringGeneratorRecursion(s);
+        System.out.println("The size is: " + res.size());
+        for (String string : res) {
             System.out.println(string);
         }
     }
@@ -29,5 +31,28 @@ class Q521Solution {
             res.add(t);
         }
         return res;
+    }
+
+    private List<String> stringGeneratorRecursion(String s) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(res, sb, s, 0);
+        return res;
+    }
+
+    private void helper(List<String> res, StringBuilder sb, String s, int startIndex) {
+        if (startIndex >= s.length()) {
+            return;
+        }
+        sb.append(s.charAt(startIndex));
+        if (startIndex == s.length() - 1) {
+            res.add(sb.toString());
+        }
+        helper(res, sb, s, startIndex + 1);
+        sb.deleteCharAt(sb.length() - 1);
+        if (startIndex == s.length() - 1) {
+            res.add(sb.toString());
+        }
+        helper(res, sb, s, startIndex + 1);
     }
 }
