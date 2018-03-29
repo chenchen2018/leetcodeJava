64eba9a962bf791d39b8bd5d9c55b375dc8e22c2
Chen Chen <Chen.Chen@tradingscreen.com>
2018-03-29 12:36:34 -0400

    Q443: add solution

---
+++ src/leetcode/easy/Q443.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Q443 {
+}
+
+class Q443Solution {
+    public int compress(char[] chars) {
+        int anchor = 0, write = 0;
+        for (int read = 0; read < chars.length; read++) {
+            if (read + 1 == chars.length || chars[read] != chars[read + 1]) {
+                chars[write++] = chars[anchor];
+                if (read > anchor) {
+                    for (char c : ("" + (read - anchor + 1)).toCharArray()) {
+                        chars[write++] = c;
+                    }
+                }
+                anchor = read + 1;
+            }
+        }
+        return write;
+    }
+}
