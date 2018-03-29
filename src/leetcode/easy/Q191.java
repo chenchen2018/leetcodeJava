82763e2de9aa96d3a76ef27eabd7a0e5c7169c20
Chen Chen <Chen.Chen@tradingscreen.com>
2018-03-29 12:40:57 -0400

    Q191: add solution

---
+++ src/leetcode/easy/Q191.java
@@ -0,0 +1,15 @@
+package leetcode.easy;
+
+public class Q191 {
+}
+
+class Q191Solution {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += (n & 1);
+            n >>>= 1;
+        }
+        return res;
+    }
+}
