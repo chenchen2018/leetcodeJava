d2c7e85c369185db132e1f2fb2d030460bc79d69
Chen Chen <chenchen@Chens-MacBook-Pro.local>
2018-04-07 12:21:03 -0400

    Q162: add solution

---
+++ src/leetcode/medium/Q162.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+public class Q162 {
+}
+
+class Q162Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+
+    public int findPeakElement2(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return r;
+    }
+}
